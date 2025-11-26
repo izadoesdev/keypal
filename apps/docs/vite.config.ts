0fe8676c54f202fb03d5b10f701eec0c592853ac
iza <59828082+izadoesdev@users.noreply.github.com>
2025-11-26 12:21:33 +0200

    del start command

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    outDir: '.output',
+  },
   plugins: [
     mdx(await import('./source.config')),
     tailwindcss(),
@@ -18,7 +21,6 @@ export default defineConfig({
     tanstackStart({
       prerender: {
         enabled: true,
-        
       },
     }),
     react(),
